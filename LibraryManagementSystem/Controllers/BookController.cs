4c6e999c2482fd76ac6e08f59f6e07754d7ce1bb
Özlem Şahin <65085413+ozlemsahin@users.noreply.github.com>
2021-10-19 20:22:32 +0300

    bookPart

--- LibraryManagementSystem/Controllers/BookController.cs
+++ LibraryManagementSystem/Controllers/BookController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LibraryManagementSystem.Models.Entity;
+
 
 namespace LibraryManagementSystem.Controllers
 {
     public class BookController : Controller
     {
+        DB_LIBRARYEntities db = new DB_LIBRARYEntities();
         // GET: Book
         public ActionResult Index()
         {
-            return View();
+            var books = db.BOOK_TABLE.ToList();
+            return View(books);
         }
     }
 }
