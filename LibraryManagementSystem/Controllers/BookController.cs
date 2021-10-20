0b68daad9b1b3fb8962e2e5217a1ecfb9f42cf9c
Özlem Şahin <65085413+ozlemsahin@users.noreply.github.com>
2021-10-20 11:33:52 +0300

    bookPart4
    book update finished

--- LibraryManagementSystem/Controllers/BookController.cs
+++ LibraryManagementSystem/Controllers/BookController.cs
@@ -48,5 +48,43 @@ namespace LibraryManagementSystem.Controllers
             return RedirectToAction("Index");
             
         }
+
+        public ActionResult DeleteBook(int id)
+        {
+            var book = db.BOOK_TABLE.Find(id);
+            db.BOOK_TABLE.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult GetBook(int id)
+        {
+            var book = db.BOOK_TABLE.Find(id);
+            //get category from its table and send to page with viewbag
+            var categoryList = db.CATEGORY_TABLE.ToList();
+            List<SelectListItem> categoryItem = (List<SelectListItem>)(from category in categoryList select new SelectListItem { Text = category.NAME, Value = category.ID.ToString() }).ToList();
+            ViewBag.categoryItem = categoryItem;
+
+            //get author from its table and send to page with viewbag
+            var authorList = db.AUTHOR_TABLE.ToList();
+            List<SelectListItem> authorItem = (List<SelectListItem>)(from author in authorList select new SelectListItem { Text = author.NAME + " " + author.SURNAME, Value = author.ID.ToString() }).ToList();
+            ViewBag.authorItem = authorItem;
+
+            return View("GetBook", book);
+        }
+
+        public ActionResult UpdateBook(BOOK_TABLE bookObj) //makes post action
+        {
+            var book = db.BOOK_TABLE.Find(bookObj.ID);
+            book.NAME = bookObj.NAME;
+            var category = db.CATEGORY_TABLE.Where(c => c.ID == bookObj.CATEGORY_TABLE.ID).FirstOrDefault();
+            book.CATEGORY = category.ID;
+            var author = db.AUTHOR_TABLE.Where(a => a.ID == bookObj.AUTHOR_TABLE.ID).FirstOrDefault();
+            book.AUTHOR = author.ID;
+            book.PRINTDATE = bookObj.PRINTDATE;
+            book.PAGE = bookObj.PAGE;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
