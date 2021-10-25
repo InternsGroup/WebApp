58953ee658635ec79e83719d0c5d663c921131fb
Özlem Şahin <65085413+ozlemsahin@users.noreply.github.com>
2021-10-25 17:04:10 +0300

    lendBook part1
    database'ye eklenmiyor, tahminimce post işlemini yapan ile button arasındaki ilişkiyi yapamadım

---
+++ LibraryManagementSystem/Controllers/LendController.cs
@@ -0,0 +1,34 @@
+﻿using LibraryManagementSystem.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class LendController : Controller
+    {
+        // GET: Lend
+        DB_LIBRARYEntities db = new DB_LIBRARYEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Lend()
+        {
+            return View();
+        }
+
+
+        //AddCategory.cshtml sayfasinda add category butonuna basilinca "post istegi" calisir ve httppost icin belirtilmis AddCategory() metodu calisir
+        [HttpPost]
+        public ActionResult AddLending(ACTION_TABLE lendObj)
+        {
+            db.ACTION_TABLE.Add(lendObj);
+            db.SaveChanges();
+            return RedirectToAction("Lend");
+        }
+    }
+}
