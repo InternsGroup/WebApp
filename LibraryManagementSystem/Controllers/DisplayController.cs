30b32dd2494d7bd0e00d8b4c3bb91cd1ac78af8e
Özlem Şahin <65085413+ozlemsahin@users.noreply.github.com>
2021-11-01 18:10:04 +0300

    display update2

--- LibraryManagementSystem/Controllers/DisplayController.cs
+++ LibraryManagementSystem/Controllers/DisplayController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LibraryManagementSystem.Models.Entity;
 
 namespace LibraryManagementSystem.Controllers
 {
     public class DisplayController : Controller
     {
+        DB_LIBRARYEntities db = new DB_LIBRARYEntities();
         // GET: Display
         public ActionResult Index()
         {
-            return View();
+            var books = db.BOOK_TABLE.ToList();
+
+            return View(books);
         }
     }
 }
