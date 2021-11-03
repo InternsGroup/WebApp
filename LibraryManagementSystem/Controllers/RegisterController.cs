08a6342ac6829de0cdf725c0081934542de3075b
Özlem Şahin <65085413+ozlemsahin@users.noreply.github.com>
2021-11-03 18:32:48 +0300

    yukleyemediklerim

---
+++ LibraryManagementSystem/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagementSystem.Models.Entity;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class RegisterController : Controller
+    {
+        DB_LIBRARYEntities db = new DB_LIBRARYEntities();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(MEMBER_TABLE userObj)
+        {
+            db.MEMBER_TABLE.Add(userObj);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
