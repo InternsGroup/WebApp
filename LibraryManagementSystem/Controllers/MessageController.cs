f006980456430e36d6229d2cbf3f82855340e219
Muhammet Ertaş <e190503054@stud.tau.edu.tr>
2021-11-05 01:15:45 +0300

    sendMessagePart
    mesaj gönderme kısmı tamam, giden mesajlar sayfası yapılacak

--- LibraryManagementSystem/Controllers/MessageController.cs
+++ LibraryManagementSystem/Controllers/MessageController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LibraryManagementSystem.Models.Entity;
 
 namespace LibraryManagementSystem.Controllers
 {
     public class MessageController : Controller
     {
         // GET: Message
+        DB_LIBRARYEntities db = new DB_LIBRARYEntities();
         public ActionResult Index()
         {
+            //session userName uzerinden yaptigimiz icin sender, receiver bilgisi email yerine userName yaptim
+            //isterseniz email ile degistirebiliriz
+            var userName = (string)Session["Username"].ToString();
+            var messages = db.MESSAGE_TABLE.Where(x => x.RECEIVER == userName.ToString()).ToList();
+            return View(messages);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(MESSAGE_TABLE messageObj)
+        {
+            var userName = (string)Session["Username"].ToString();
+            messageObj.SENDER = userName.ToString();
+            messageObj.DATE = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.MESSAGE_TABLE.Add(messageObj);
+            db.SaveChanges();
             return View();
         }
     }
